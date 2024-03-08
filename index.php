3ae34825ee39ae334bcda0a74d5e7411d13c3c30
Lea Kibur <lea.kibur@tptlive.ee>
2024-03-08 11:01:04 +0200

    Static Function: Static & Self

--- index.php
+++ index.php
@@ -1,45 +1,43 @@
 <?php
 // plug-in: PHP extension pack ja PHP Intelephense
 
+class Box {
+    public $color;
+    public $size;
+    public static $number;
 
-//library - me saime sellise klassi
-
-class Job {
-    public function task(Logger $logger){
-            for($i = 0; $i < 10; $i++){
-            $logger->log("task done! #$i\n");
-
-// kahekordsed jutumärgid, kui tahame kasutada \n (line break)
-// või muid spetsiaalseid stringi funktsioone
-        }
+//staatilise funtsiooni sees ma ei tohi kasutada võtmesõna "this"
+//ma ei saa viidata ühelegi mitte staatilisele muutujale
+    public static function getNumber(){
+        echo self::$number;
     }
-}
-interface Logger {
-    public function log($message);
-}
-
-class ConsoleLogger implements Logger{
-    public function log($message){
-        echo $message . "\n";
+    public function getSize(){
+        echo $this->size . self::$number;
     }
-}
-
-// usage code - me tahame, et see klass teeks mingeid asju
-
-class NothingLogger implements Logger{
-    public function log($message){
-
+    public static function me1(){
+        var_dump(self::class);
+//class on staatiline komponent, see annab alati tekstina klassi nime
+    }
+    public static function me2(){
+        var_dump(static::class);
     }
 }
 
-class FileLogger implements Logger{
-    public function log($message){
-        $file = fopen("log.txt", "a");
-        fwrite($file, $message . "\n");
-        fclose($file);
-    }
+class MetalBox extends Box{
+
 }
 
-$job = new Job();
-$logger = new FileLogger;
-$job->task($logger);
+$box1 = new Box();
+$box1->size = 10;
+Box::$number = 12;
+$box1->getSize();
+Box::getNumber();
+//$box2 = new Box();
+//$box2->size = 20;
+Box::$number = 20;
+//var_dump($box1, $box2);
+var_dump(Box::$number, Box::$number);
+box::me1();
+box::me2();
+MetalBox::me1();
+MetalBox::me2();
